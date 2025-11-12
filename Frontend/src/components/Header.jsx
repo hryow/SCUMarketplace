6a20434e28fd2bbbc0fe5fec14e6c65a2543be12
Thai <161995478+thehobbes@users.noreply.github.com>
2025-11-12 13:44:20 -0800

    Remove unused code

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -12,15 +12,5 @@ export default function Header(){
                 <div>sell something</div>
             </div>
         </div>
-        // <header className="siteHeader">
-        //     <nav>
-        //         <ul>
-        //             <li><Link to="/profile">profile</Link></li>
-        //             <li><Link to="/Gallery">Gallery</Link></li>
-        //             <li><Link to="/Listing">Create Listing</Link></li>
-        //         </ul>
-        //     </nav>
-
-        // </header>
     )
 }
