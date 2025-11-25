6cf42cc00ada5f8c91c2998b76ed45c3d5b0a1db
Hayoung Ryow <hayoung.ryow@gmail.com>
2025-11-24 20:45:44 -0800

    ci: add GitHub Actions test workflow

---
+++ Server/jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  testEnvironment: 'node',
+  setupFilesAfterEnv: ['./tests/setup.js'],
+  testMatch: ['**/tests/**/*.test.js'],
+  coveragePathIgnorePatterns: ['/node_modules/']
+};
