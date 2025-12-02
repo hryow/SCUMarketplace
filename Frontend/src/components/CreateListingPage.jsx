f5fdccd57634ae9319e5a95acb55045ace2465fa
Thai <161995478+thehobbes@users.noreply.github.com>
2025-12-01 16:22:57 -0800

    Fix bug of Header tweaking out

--- Frontend/src/components/CreateListingPage.jsx
+++ Frontend/src/components/CreateListingPage.jsx
@@ -102,6 +102,8 @@ else{
 }
 
 return (
+    <>
+        <Header userEmail={userEmail} />
         <motion.div
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
@@ -109,7 +111,6 @@ return (
             transition={{ duration: 0.3 }}
             style={{ width: '100%', height: '100%' }}
         >
-            <Header userEmail={userEmail} />
             <div className={styles.pageContainer}> 
             <Link to="/Gallery" className={styles.backButton}>
             &lt; Back
@@ -159,5 +160,6 @@ return (
                 </form>
             </div>
         </motion.div>
+    </>
     );
 }
--- Frontend/src/components/GalleryPage.jsx
+++ Frontend/src/components/GalleryPage.jsx
@@ -24,21 +24,23 @@ export default function GalleryPage({ userEmail }) {
     }, []); // runs once on load
 
     return (
-        <motion.div
-            initial={{ opacity: 0, x: 100 }}
-            animate={{ opacity: 1, x: 0 }}
-            exit={{ opacity: 0, x: -100 }}
-            transition={{ duration: 0.3 }}
-        >
+        <>
             <Header userEmail={userEmail}/>
-            <div className={styles.gridContainer}>
-                {listings.map((listing) => (
-                    <ListingCard 
-                        key={listing.id}
-                        listingData = {listing}
-                    />
-                ))}
-            </div>
-        </motion.div>
+            <motion.div
+                initial={{ opacity: 0, x: 100 }}
+                animate={{ opacity: 1, x: 0 }}
+                exit={{ opacity: 0, x: -100 }}
+                transition={{ duration: 0.3 }}
+            >
+                <div className={styles.gridContainer}>
+                    {listings.map((listing) => (
+                        <ListingCard 
+                            key={listing.id}
+                            listingData = {listing}
+                        />
+                    ))}
+                </div>
+            </motion.div>
+        </>
     );
 }
