0d91ad1622b04da089e2ca04ad226c710351b0f2
Thai <161995478+thehobbes@users.noreply.github.com>
2025-12-04 12:50:10 -0800

    Create clickable logo

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from './Header.module.css';
-
-/* TODO: make button pressable (goes down on click)
-         make "User" depend on name 
-         make pfp depend on pfp 
-         make add new listing button go to create listing page*/
+import logo from '../assets/scu-marketplace-full-logo.png';
 
 export default function Header({ userEmail }){
     return(
         <div className={styles.container}>
-            <div className={styles.profile}></div>
+            <Link to="/Gallery" className={styles.logo}>
+                <img src={logo} alt="logo" className={styles.logoImage} />
+            </Link>
             <div className={styles.welcome}>Welcome, {userEmail}!</div>
             <Link to="/listing" className={styles.addListingButton}>
                 <div className={styles.plus}>+</div>
