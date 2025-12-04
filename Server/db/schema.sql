4ea00223c25c4a5208ba5605be13ccaffd200039
Trisha Ganesh <43594876+trishaganesh@users.noreply.github.com>
2025-12-03 23:56:02 -0800

    Remove comma, will throw syntax error

--- Server/db/schema.sql
+++ Server/db/schema.sql
@@ -14,7 +14,7 @@
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     email VARCHAR(255) UNIQUE NOT NULL CHECK (email LIKE '%@scu.edu'),
-    password VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
 );
 
 
