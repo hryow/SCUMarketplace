dcfb7634f5972e58613970a2aecfe16e4fc7def5
Trisha Ganesh <43594876+trishaganesh@users.noreply.github.com>
2025-12-04 00:23:40 -0800

    Add missing routes

--- Server/routes/routes.js
+++ Server/routes/routes.js
@@ -6,15 +6,18 @@ const controller = require('../controllers/controller');
 
 // Route to create a new user
 router.post('/createuser', controller.createUser);
-
 // Route for user login
 router.post('/login', controller.login);
 
 // Route to create a new listing
 router.post('/createlisting', controller.createListing);
-
 // Route for fetching all listings
 router.get('/getlistings', controller.getListings);
+router.get('/getlistings/:id', controller.getListingById);
+router.delete('/deletelisting/:id', controller.deleteListing);
 
 // Export router to use (index.js)
 module.exports = router;
+
+
+
